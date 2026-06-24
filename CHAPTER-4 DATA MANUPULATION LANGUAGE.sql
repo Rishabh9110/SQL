e24a07d870eb1cd945ce59324bd8cae9dbcb9950
Rishabh91110 <rrishabh283@gmail.com>
2026-06-24 14:51:11 +0530

    updating queries

---
+++ CHAPTER-4 DATA MANUPULATION LANGUAGE.sql
@@ -0,0 +1,21 @@
+--CHAPTER 4 - DATA MANUPULATION LANGUAGE
+
+--METHOD 1 OF INSERTING DATA INTO TABLES
+INSERT INTO customers  (id,first_name,country,score)
+VALUES (6,'Anna','USA',NULL),
+       (7,'Sam',NULL,100),
+      (8,'USA','MAX',NULL),
+       (9,'Andreas','Germany',null),
+       (10,'Sahra',null,null)
+--METHOD 2- INSERTING DATA INTO TABLES FROM ANOTHER TABLE CALLED SOURCE TABLE
+INSERT INTO persons (id,persons_name,birth_date,phone)
+SELECT id,first_name,null,'unknown' from customers
+
+--TASK 1-CHANGE THE SCORE OF CUSTOMER 6 TO 0
+UPDATE customers SET score=0 WHERE ID =6;
+
+
+
+
+
+
