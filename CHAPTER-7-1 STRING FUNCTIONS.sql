2b78a656bdf665818ae26150ed49bec24c423b73
Rishabh91110 <rrishabh283@gmail.com>
2026-07-07 19:11:26 +0530

    updating query

--- CHAPTER-7-1 STRING FUNCTIONS.sql
+++ CHAPTER-7-1 STRING FUNCTIONS.sql
@@ -1,10 +1,12 @@
 --CHAPTER-7-1 STRING FUNCTIONS
 
 --TASK1-1-CONCATINATE FIRST NAME AND COUNTRY INTO ONE COLUMN
-SELECT first_name,concat(first_name,' ',country) AS NAME_COUNTRY FROM customers
+SELECT CONCAT(first_name,country) AS NAME_COUNTRY FROM customers; 
 --Task-2-convert firstname to upper case
-SELECT upper(first_name) AS name_capital FROM customers;
+SELECT UPPER(first_name) AS NAME_IN_CAPITAL FROM customers;
 --Task-3-find customer whose first name contains leading and trailing spaces
-SELECT first_name FROM customers WHERE first_name != TRIM(first_name)
+SELECT first_name FROM Customers WHERE first_name!=TRIM(first_name);
 --Task-4-Remove dashes from a static value 
-SELECT '123-456-789' AS PHONE,REPLACE('123-456-789','-','/') AS CLEAN_PHONE
+SELECT '123-456-789' AS PHONE_NUMBER,REPLACE('123-456-789','-','/') AS new_phone_number;
+--task-5-Calculate the length of each customer first name
+SELECT first_name,LEN(first_name) AS NAME_LENGTH FROM customers; 
